4216cee740126c4818101f6a2fc37923890155d3
Chris Thomas <chris.thomas@antimatter-studios.com>
2019-09-04 23:53:05 +0200

    pass the token to the metadata service

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -12,16 +12,6 @@ use Illuminate\Support\ServiceProvider;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
      * Boot the authentication services for the application.
      *
      * @return void
