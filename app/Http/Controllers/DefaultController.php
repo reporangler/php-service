7c3b30150b2466383408b84a6855b39c221dde7e
Chris Thomas <christopher.thomas@plista.com>
2020-05-24 23:32:12 +0200

    revert the last change, I tested it and I hated it.

--- app/Http/Controllers/DefaultController.php
+++ app/Http/Controllers/DefaultController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Laravel\Lumen\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Http\JsonResponse;
 
 class DefaultController extends BaseController
 {
-    public function cors($args)
-    {
-        return $this->healthz();
-    }
-
     public function healthz()
     {
-        return new JsonResponse([
-            "statusCode" => 200,
-            "service" => config('app.php_base_url')
-        ]);
+        return new JsonResponse(["statusCode" => 200, "service" => config('app.php_base_url')], 200);
+    }
+
+    public function cors()
+    {
+        $this->healthz();
     }
 }
